80ea14226d5846202846c4859e5927165ac3e4bd
jessie <a0915655212@gmail.com>
2016-08-19 14:09:25 +0800

    1.移除bin和obj 2.新增開發案資料夾

---
+++ SetPoint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SetPoint.MvcApplication" Language="C#" %>
---
+++ SetPoint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SetPoint.Startup))]
+namespace SetPoint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
