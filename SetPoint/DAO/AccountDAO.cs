37f9674398f4bce330d3aa879555545209c37f3f
WayneShenHH <hh4854@gmail.com>
2016-08-20 15:47:07 +0800

    add dao add connection add login

---
+++ SetPoint/DAO/AccountDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SetPoint.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using SetPoint.Utility;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNet.Identity.Owin;
+namespace SetPoint.DAO
+{
+    public class AccountDAO
+    {
+        class UserInfo
+        {
+            public string _id;
+            public string name;
+            public string uid;
+            public string pwd;
+        }
+        public static async Task<SignInStatus> Login(LoginViewModel model)
+        {
+            var builder = Builders<BsonDocument>.Filter;
+            var filter = builder.Eq("uid", model.Email) & builder.Eq("pwd", model.Password);
+            var resultList = await MongoDb.QueryDB(filter, "Account");
+            
+            var result = SignInStatus.RequiresVerification;
+            if (resultList != null && resultList.Count > 0)
+            {
+                if (resultList[0].GetElement("uid").Value == model.Email)
+                {
+                    result = SignInStatus.Success;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ SetPoint/Utility/MongoDb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SetPoint.Utility
+{
+    public class MongoDb
+    {
+        static string connectionString = ConfigurationManager.ConnectionStrings["MongoConnection"].ConnectionString;// "mongodb://localhost";
+        static string databaseName = "db_dev";
+        public static async Task<List<BsonDocument>> QueryDB(FilterDefinition<BsonDocument> filter,string collection)
+        {
+            var _client = new MongoClient(connectionString);
+            var _database = _client.GetDatabase(databaseName);
+            var _collection = _database.GetCollection<BsonDocument>(collection);
+            var result = await  _collection.Find(filter).ToListAsync();
+            return result;
+        }
+    }
+}
